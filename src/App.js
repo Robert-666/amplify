99edb23826cd69077519743acae369a4eb5904a2
Robert-666 <robertavetisyan673@gmail.com>
2023-02-19 20:10:23 +0400

    to github

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { Analytics } from 'aws-amplify';
 import { useEffect } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({ signOut, user }) {
   useEffect(() => {
     Analytics.record('App component mounted');
   }, []);
+  const formFields = {
+    signUp: {
+      phone_number: {
+        dialCode: '+374',
+      },
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload with AWS amplify.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator
+      signUpAttributes={[
+        'address',
+        'birthdate',
+        'email',
+        'family_name',
+        'gender',
+        'given_name',
+        'locale',
+        'middle_name',
+      ]}
+      formFields={formFields}
+    >
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,15 +5,25 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_identity_pool_id": "us-east-1:1443fbc1-7578-4568-bacb-46dff25515b6",
     "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_xG0YZPzNb",
+    "aws_user_pools_web_client_id": "6up3j6fnmbha6851kgq938130",
     "oauth": {},
     "aws_cognito_username_attributes": [],
     "aws_cognito_social_providers": [],
-    "aws_cognito_signup_attributes": [],
-    "aws_cognito_mfa_types": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
     "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
         "passwordPolicyCharacters": []
     },
-    "aws_cognito_verification_mechanisms": [],
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ],
     "aws_mobile_analytics_app_id": "067440be81674017a646649df613ef73",
     "aws_mobile_analytics_app_region": "us-east-1"
 };
